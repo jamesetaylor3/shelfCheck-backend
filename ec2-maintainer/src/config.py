2210a752fedc932bd811bafe99bde9bf8693c2c6
James Taylor <jamestaylor3@protonmail.com>
2020-05-21 14:21:15 -0400

    added all items to web crawler, adjusted stock values

--- ec2-maintainer/src/config.py
+++ ec2-maintainer/src/config.py
@@ -1,7 +1,5 @@
-items = [
-	"Bread",
-	"Toilet Paper"
-]
+all_items = ["Bread", "Milk", "Eggs", "Bottled Water", "Ground Beef", "Toilet Paper", "Diapers", "Masks",
+             "Garbage Bags", "Disinfectant Wipes", "Hand Sanitizer", "Hand Soap", "Paper Towels", "Batteries", "Flashlights"]
 
 zip_codes = [
 	"27513"
